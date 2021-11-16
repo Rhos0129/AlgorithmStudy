e8e11cc460c4e53bf6b161baa08028dec55793a3
jin <rhos0129@gmail.com>
2021-11-16 23:14:59 +0900

    Lv2 : 흥미로운 풀이 우선순위 역순으로 괄호를 넣어 계산

--- 프로그래머스/Lv2/[카카오] 수식 최대화.py
+++ 프로그래머스/Lv2/[카카오] 수식 최대화.py
@@ -1,29 +1,13 @@
-import re
 from itertools import permutations
 
-
 def solution(expression):
-
-    # 숫자와 사칙연산 분리
-    ex=re.split('(\d+)', expression)[1:-1]
-
-    # ex에 있는 연산자 파악(중복X)
-    operators=[]
-    for i in ex:
-        if not i.isdigit() and i not in operators:
-            operators.append(i)
-
-    # 연산자 우선순위마다 값 계산
-    results=[]
-    for order in permutations(operators, len(operators)):
-        ex_=ex.copy()
-        for o in order:
-            while o in ex_:
-                oIdx=ex_.index(o)
-                ex_[oIdx-1]=str(eval(''.join(ex_[oIdx-1:oIdx+2])))
-                ex_.pop(oIdx+1)
-                ex_.pop(oIdx)
-
-        results.append(abs(int(ex_[0])))
-
-    return max(results)
+    operations = permutations(['-','+','*'], 3)
+    answer = []
+    for op in operations:
+        a, b, c=op[0], op[1], op[2]
+        result = []
+        for aSplit in expression.split(a):
+            bSplit = [f"({i})" for i in aSplit.split(b)]
+            result.append(f'({b.join(bSplit)})')
+        answer.append(abs(eval(a.join(result))))
+    return max(answer)

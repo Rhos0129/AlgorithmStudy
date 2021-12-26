976e1bce0ef4457c156f5cfc9aae895dce26ad99
jin <rhos0129@gmail.com>
2021-12-27 00:21:35 +0900

    Lv1 : 모범답안 정규표현식

--- 프로그래머스/Lv1/[카카오] 다트게임.py
+++ 프로그래머스/Lv1/[카카오] 다트게임.py
@@ -1,26 +1,14 @@
+import re
+
 def solution(dartResult):
-    answer = []
-    dartResult = dartResult.replace('10', 't')
-    dartResult = list(dartResult)
-    for i in range(len(dartResult)):
-        if dartResult[i] == 't':
-            dartResult[i] = '10'
+    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+    option = {'' : 1, '*' : 2, '#' : -1}
+    p = re.compile('(\d+)([SDT])([*#]?)')
+    dart = p.findall(dartResult)
+    for i in range(len(dart)):
+        if dart[i][2] == '*' and i > 0:
+            dart[i-1] *= 2
+        dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]]
 
-    for i in range(1, len(dartResult)):
-        if dartResult[i] == "S":
-            answer.append(int(dartResult[i - 1]))
-        elif dartResult[i] == "D":
-            answer.append(int(dartResult[i - 1]) ** 2)
-        elif dartResult[i] == "T":
-            answer.append(int(dartResult[i - 1]) ** 3)
-
-        if dartResult[i] == "*":
-            if len(answer) >= 2:
-                answer[-1] = answer[-1] * 2
-                answer[-2] = answer[-2] * 2
-            else:
-                answer[-1] = answer[-1] * 2
-        elif dartResult[i] == '#':
-            answer[-1] = answer[-1] * (-1)
-
-    return sum(answer)
+    answer = sum(dart)
+    return answer

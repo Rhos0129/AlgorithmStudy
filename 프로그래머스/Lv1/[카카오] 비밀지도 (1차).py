c4705893999dbd9046a265943e870c4b86111cbf
jin <rhos0129@gmail.com>
2021-12-26 23:56:17 +0900

    Lv1 : 모범답안 zip, rjust

--- 프로그래머스/Lv1/[카카오] 비밀지도 (1차).py
+++ 프로그래머스/Lv1/[카카오] 비밀지도 (1차).py
@@ -1,31 +1,9 @@
 def solution(n, arr1, arr2):
     answer = []
-
-    li1 = []
-    for i in arr1:
-        tmp = format(i, 'b')
-        while len(tmp) < n:
-            tmp = '0' + tmp
-        li1.append(tmp)
-    # print(li1)
-
-    li2 = []
-    for i in arr2:
-        tmp = format(i, 'b')
-        while len(tmp) < n:
-            tmp = '0' + tmp
-        li2.append(tmp)
-    # print(li2)
-
-    for i in range(n):
-        tmp = ''
-        for j in range(n):
-            # print(li1[i], li2[i])
-            if li1[i][j:j + 1] == '0' and li2[i][j:j + 1] == '0':
-                tmp += ' '
-            else:
-                tmp += '#'
-            # print(tmp)
-        answer.append(tmp)
-
+    for i,j in zip(arr1,arr2):
+        a12 = str(bin(i|j)[2:])
+        a12=a12.rjust(n,'0')
+        a12=a12.replace('1','#')
+        a12=a12.replace('0',' ')
+        answer.append(a12)
     return answer

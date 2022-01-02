0551fd2ceaafc816144219e9b984e79101e57d5d
jin <rhos0129@gmail.com>
2022-01-02 20:02:56 +0900

    Lv1

---
+++ 프로그래머스/Lv1/[카카오] 숫자 문자열과 영단어.py
@@ -0,0 +1,9 @@
+def solution(s):
+    li = [['0', 'zero'], ['1', 'one'], ['2', 'two'], ['3', 'three'], ['4', 'four'],
+          ['5', 'five'], ['6', 'six'], ['7', 'seven'], ['8', 'eight'], ['9', 'nine']]
+
+    for i, j in li:
+        if j in s:
+            s = s.replace(j, i)
+
+    return int(s)

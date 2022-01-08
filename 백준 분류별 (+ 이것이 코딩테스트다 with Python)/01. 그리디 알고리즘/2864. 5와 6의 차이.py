a17a96167dfe1b1dc6244b69305e56d8259954d6
jin <rhos0129@gmail.com>
2022-01-08 17:33:19 +0900

    브론즈 2 : 간단한 풀이

--- 백준 분류별 (+ 이것이 코딩테스트다 with Python)/01. 그리디 알고리즘/2864. 5와 6의 차이.py
+++ 백준 분류별 (+ 이것이 코딩테스트다 with Python)/01. 그리디 알고리즘/2864. 5와 6의 차이.py
@@ -1,20 +1,2 @@
-num1, num2=input().split()
-
-def lowhigh(num):
-    lowNum=0; highNum=0
-    for idx, i in enumerate(num):
-        place = len(num) - idx - 1
-        if i=='5' or i=='6':
-            lowNum+=5*(10**place)
-            highNum+=6*(10**place)
-        else:
-            lowNum+=int(i)*(10**place)
-            highNum+=int(i)*(10**place)
-    return lowNum, highNum
-
-for i, j in zip(lowhigh(num1), lowhigh(num2)):
-    print(i+j, end=' ')
-
-
-
-
+A, B = input().split()
+print(int(A.replace('6', '5'))+int(B.replace('6', '5')), int(A.replace('5', '6'))+int(B.replace('5', '6')))
